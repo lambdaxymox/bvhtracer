5bd5e73860511e1dcfd75e8c19f7019a155f0ffa
LambdaXymox <lambda.xymox@gmail.com>
2022-06-20 16:26:03 -0500

    Optimize intersect_aabb.

--- src/bvhtracer.rs
+++ src/bvhtracer.rs
@@ -57,12 +57,19 @@ impl Triangle {
 pub struct Ray {
     pub origin: Vector3<f32>,
     pub direction: Vector3<f32>,
+    pub recip_direction: Vector3<f32>,
     pub t: f32,
 }
 
 impl Ray {
     pub fn new(origin: Vector3<f32>, direction: Vector3<f32>, t: f32) -> Self {
-        Self { origin, direction, t, }
+        let recip_direction = Vector3::new(
+            1_f32 / direction.x, 
+            1_f32 / direction.y, 
+            1_f32 / direction.z
+        );
+
+        Self { origin, direction, recip_direction, t, }
     }
 
     pub fn from_origin_dir(origin: Vector3<f32>, direction: Vector3<f32>) -> Self {
