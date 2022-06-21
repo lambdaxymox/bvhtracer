d8fb11267f0ddd794f39eb7a9d35fa7e49a30e11
LambdaXymox <lambda.xymox@gmail.com>
2022-06-20 21:21:42 -0500

    Remove dead code.

--- tri_loader/src/lib.rs
+++ tri_loader/src/lib.rs
@@ -30,37 +30,6 @@ pub fn from_reader<R: io::Read + io::Seek>(reader: &mut R) -> Result<Vec<Triangl
     reader.read_to_string(&mut contents).unwrap();
     let mut loader = TriLoader::new(&contents);
     loader.load()
-    /*
-    let mut triangles = vec![];
-    for line in contents.split('\n') {
-        let mut elements = [0_f32; 9];
-        for (i, element_i) in line.split(' ').enumerate() {
-            elements[i] = match element_i.parse::<f32>() {
-                Ok(value) => value,
-                Err(_) => return Err(TriLoaderError::new(
-                    0,
-                    ErrorKind::ExpectedFloat,
-                    format!("Expected a floating point number but got `{}` instead.", element_i)
-                )),
-            };
-        }
-
-        let mut vertices = [Vector3::zero(); 3];
-        for (i, vertex_i) in elements
-            .chunks(3)
-            .map(|chunk| Vector3::new(chunk[0], chunk[1], chunk[2]))
-            .enumerate() 
-        {
-            vertices[i] = vertex_i;
-        }
-
-        let triangle = Triangle::new(vertices[0], vertices[1], vertices[2]);
-
-        triangles.push(triangle);
-    }
-
-    Ok(triangles)
-    */
 }
 
 
