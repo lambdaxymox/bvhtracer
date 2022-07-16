7efc870aa562a0cd83190b5366c22f85dc01853a
LambdaXymox <lambda.xymox@gmail.com>
2022-07-15 19:08:28 -0500

    Add tests for triangle intersection.

--- tests/test_triangle_intersection.rs
+++ tests/test_triangle_intersection.rs
@@ -32,6 +32,42 @@ fn test_triangle_intersection_hits() {
 }
 
 #[test]
+fn test_triangle_intersection_vertex_hits1() {
+    let scene = scene();
+    let vertex = Vector3::new(0_f32, 1_f32 / 2_f32, 0_f32);
+    let ray_origin = Vector3::new(0_f32, 0_f32, 5_f32);
+    let ray_direction = (vertex - ray_origin).normalize();
+    let ray = Ray::from_origin_dir(ray_origin, ray_direction);
+    let result = scene.intersect(&ray);
+
+    assert!(result.is_some());
+}
+
+#[test]
+fn test_triangle_intersection_vertex_hits2() {
+    let scene = scene();
+    let vertex = Vector3::new(-1_f32 / f32::sqrt(3_f32), -1_f32 / 2_f32, 0_f32);
+    let ray_origin = Vector3::new(0_f32, 0_f32, 5_f32);
+    let ray_direction = (vertex - ray_origin).normalize();
+    let ray = Ray::from_origin_dir(ray_origin, ray_direction);
+    let result = scene.intersect(&ray);
+
+    assert!(result.is_some());
+}
+
+#[test]
+fn test_triangle_intersection_vertex_hits3() {
+    let scene = scene();
+    let vertex = Vector3::new(1_f32 / f32::sqrt(3_f32), -1_f32 / 2_f32, 0_f32);
+    let ray_origin = Vector3::new(0_f32, 0_f32, 5_f32);
+    let ray_direction = (vertex - ray_origin).normalize();
+    let ray = Ray::from_origin_dir(ray_origin, ray_direction);
+    let result = scene.intersect(&ray);
+
+    assert!(result.is_some());
+}
+
+#[test]
 fn test_triangle_intersection_center() {
     let scene = scene();
     let ray_origin = Vector3::new(0_f32, 0_f32, 5_f32);
@@ -44,5 +80,92 @@ fn test_triangle_intersection_center() {
     let result = Ray::new(ray.origin, ray.direction, result_t);
 
     assert_eq!(result, expected);
+}
+
+#[test]
+fn test_triangle_intersection_vertex1() {
+    let scene = scene();
+    let vertex = Vector3::new(0_f32, 1_f32 / 2_f32, 0_f32);
+    let ray_origin = Vector3::new(0_f32, 0_f32, 5_f32);
+    let ray_direction = (vertex - ray_origin).normalize();
+    let ray = Ray::from_origin_dir(ray_origin, ray_direction);
+    let expected_t = f32::sqrt(101_f32 / 4_f32);
+    let expected = Ray::new(ray_origin, ray_direction, expected_t);
+    let result_t = scene.intersect(&ray).unwrap();
+    let result = Ray::new(ray.origin, ray.direction, result_t);
+
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_triangle_intersection_vertex2() {
+    let scene = scene();
+    let vertex = Vector3::new(-1_f32 / f32::sqrt(3_f32), -1_f32 / 2_f32, 0_f32);
+    let ray_origin = Vector3::new(0_f32, 0_f32, 5_f32);
+    let ray_direction = (vertex - ray_origin).normalize();
+    let ray = Ray::from_origin_dir(ray_origin, ray_direction);
+    let expected_t = f32::sqrt(307_f32 / 12_f32);
+    let expected = Ray::new(ray_origin, ray_direction, expected_t);
+    let result_t = scene.intersect(&ray).unwrap();
+    let result = Ray::new(ray.origin, ray.direction, result_t);
+
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_triangle_intersection_vertex3() {
+    let scene = scene();
+    let vertex = Vector3::new(1_f32 / f32::sqrt(3_f32), -1_f32 / 2_f32, 0_f32);
+    let ray_origin = Vector3::new(0_f32, 0_f32, 5_f32);
+    let ray_direction = (vertex - ray_origin).normalize();
+    let ray = Ray::from_origin_dir(ray_origin, ray_direction);
+    let expected_t = f32::sqrt(307_f32 / 12_f32);
+    let expected = Ray::new(ray_origin, ray_direction, expected_t);
+    let result_t = scene.intersect(&ray).unwrap();
+    let result = Ray::new(ray.origin, ray.direction, result_t);
+
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_triangle_intersection_vertex_miss1() {
+    let scene = scene();
+    let vertex = Vector3::new(0_f32, 1_f32 / 2_f32, 0_f32);
+    let displacement = Vector3::new(0_f32, 1_f32 / 2_f32, 0_f32);
+    let target = vertex + displacement;
+    let ray_origin = Vector3::new(0_f32, 0_f32, 5_f32);
+    let ray_direction = (target - ray_origin).normalize();
+    let ray = Ray::from_origin_dir(ray_origin, ray_direction);
+    let result = scene.intersect(&ray);
+
+    assert!(result.is_none());
+}
+
+#[test]
+fn test_triangle_intersection_vertex_miss2() {
+    let scene = scene();
+    let vertex = Vector3::new(-1_f32 / f32::sqrt(3_f32), -1_f32 / 2_f32, 0_f32);
+    let displacement = Vector3::new(0_f32, -1_f32 / 2_f32, 0_f32);
+    let target = vertex + displacement;
+    let ray_origin = Vector3::new(0_f32, 0_f32, 5_f32);
+    let ray_direction = (target - ray_origin).normalize();
+    let ray = Ray::from_origin_dir(ray_origin, ray_direction);
+    let result = scene.intersect(&ray);
+
+    assert!(result.is_none());
+}
+
+#[test]
+fn test_triangle_intersection_vertex_miss3() {
+    let scene = scene();
+    let vertex = Vector3::new(1_f32 / f32::sqrt(3_f32), -1_f32 / 2_f32, 0_f32);
+    let displacement = Vector3::new(0_f32, -1_f32 / 2_f32, 0_f32);
+    let target = vertex + displacement;
+    let ray_origin = Vector3::new(0_f32, 0_f32, 5_f32);
+    let ray_direction = (target - ray_origin).normalize();
+    let ray = Ray::from_origin_dir(ray_origin, ray_direction);
+    let result = scene.intersect(&ray);
+
+    assert!(result.is_none());
 }
 
